42753cbeb25a7fe98589b4212b21870538cc713f
rafaelsiq <rafaelsqf2@gmail.com>
2021-03-25 23:22:51 -0300

    task/40 #40 pagina de signup com angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { SignupMenuComponent } from './components/signup-menu/signup-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupPageComponent,
+    SignupMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
