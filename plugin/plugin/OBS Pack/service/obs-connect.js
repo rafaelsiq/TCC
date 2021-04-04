19a5d5fe316f4b4cc67fa7e0d1a433f84acf33de
rafaelsiq <rafaelsqf2@gmail.com>
2021-04-04 19:20:48 -0300

    task/57 #59 Adicionada a classe de comunicação com o OBS

--- plugin/plugin/OBS Pack/service/obs-connect.js
+++ plugin/plugin/OBS Pack/service/obs-connect.js
@@ -1,1 +1,39 @@
 //connect to obs
+
+//Get Modules and Setup
+const OBSWebSocket = require('obs-websocket-js');
+const obs = new OBSWebSocket();
+
+//INPUT YOUR STUFF HERE:
+//OBS Config
+const obsIp = "127.0.0.1"
+const obsPort = 4444;
+const obsPassword = "secret"
+
+
+
+//Connect to OBS
+obs.connect({
+        address: obsIp + ':'+ obsPort,
+        password: obsPassword
+    })
+    .then(() => {
+        console.log(`\nConnected & authenticated to OBS Websockets...\nIP: ${obsIp}\nPort: ` + obsPort);
+
+        return obs.send('GetSceneList');                                    //Send a Get Scene List Promise
+    })
+    .then(data => {
+        console.log(`\n${data.scenes.length} Available Scenes.` + "\n");    //Log Total Scenes
+        console.log(data.scenes.forEach((thing, index) => {
+            console.log((index + 1) + " - " + thing.name);                  //Log List of Available Scenes with OSC Index
+        }));
+
+        console.log('-- Use "/scene [index]" For OSC Control --\n\n');      //Log OSC Scene Syntax
+    })
+    .catch(err => {
+        console.log(err);                                                   //Log Catch Errors
+        console.log("-!- Make Sure OBS is Running and Websocket IP/Port/Password are Correct -!-");
+    });
+    
+
+module.exports = obs;
